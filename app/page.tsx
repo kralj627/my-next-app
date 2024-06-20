22add9540428fa4379eec4ceb9dde934c9b03b16
[GitHub <[GitHub>
2024-06-20 11:33:20 +0900

    jpg re

--- app/page.tsx
+++ app/page.tsx
@@ -74,7 +74,7 @@ export default function Home() {
               </div>
               <h3 className="text-lg font-semibold">악!</h3>
               <p className="text-gray-500 dark:text-gray-400">
-                플레이어의 목소리 크기를 인식하여 점프하는 플랫포머 게임
+                플레이어의 목소리 크기를 인식해 점프하는 플랫포머 게임
               </p>
             </div>
             <div className="rounded-lg border border-gray-200 bg-white p-4 shadow-sm transition-all hover:shadow-md dark:border-gray-800 dark:bg-gray-950 dark:hover:shadow-lg text-center">
